004b5b1f65bd13470b529e0c1d8d10fc8160a6ce
Sebastian Reimers <info@miete-admin.de>
2016-01-13 18:20:48 +0100

    fix 10.6 weak bug

--- StudioLinkStandalone/AppDelegate.m
+++ StudioLinkStandalone/AppDelegate.m
@@ -15,7 +15,7 @@ pthread_t tid;
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+@property (unsafe_unretained) IBOutlet NSWindow *window;
 @end
 
 @implementation AppDelegate
