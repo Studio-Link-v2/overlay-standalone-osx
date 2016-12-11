b8063fb38ff95ae68d0410583f921cdf8c2bdeb5
Sebastian Reimers <info@miete-admin.de>
2016-12-11 13:23:41 +0100

    add FLAC support and update baresip api

--- StudioLinkStandalone/AppDelegate.m
+++ StudioLinkStandalone/AppDelegate.m
@@ -13,6 +13,16 @@
 
 pthread_t tid;
 
+static void ua_exit_handler(void *arg)
+{
+	(void)arg;
+	debug("ua exited -- stopping main runloop\n");
+
+	/* The main run-loop can be stopped now */
+	re_cancel();
+}
+
+
 @interface AppDelegate ()
 
 @property (unsafe_unretained) IBOutlet NSWindow *window;
@@ -25,19 +35,25 @@ pthread_t tid;
     (void)sys_coredump_set(true);
     libre_init();
     conf_configure();
+    baresip_init(conf_config(), false);
+    uag_set_exit_handler(ua_exit_handler, NULL);
     ua_init("baresip v" BARESIP_VERSION " (" ARCH "/" OS ")", true, true, true, false);
     conf_modules();
-    
+    uag_set_exit_handler(ua_exit_handler, NULL);    
     pthread_create(&tid, NULL, (void*(*)(void*))&re_main, NULL);
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
     ua_stop_all(false);
-    (void)pthread_join(tid, NULL);
+    sys_msleep(500);
     ua_close();
+    conf_close();
+    baresip_close();
     mod_close();
     libre_close();
+    tmr_debug();
+    mem_debug();
 }
 
 @end
